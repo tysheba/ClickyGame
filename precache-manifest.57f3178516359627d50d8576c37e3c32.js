e97be4616f43e0020618559fbdfa4b7ad4290c42
Tysheba <tmorgan2dev@gmail.com>
2018-11-17 00:16:27 -0500

    Updates

---
+++ precache-manifest.57f3178516359627d50d8576c37e3c32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "59843ae1c2ed89599fd0",
+    "url": "/ClickyGame/static/js/runtime~main.59843ae1.js"
+  },
+  {
+    "revision": "080ae0317c8ac19e92c0",
+    "url": "/ClickyGame/static/js/main.080ae031.chunk.js"
+  },
+  {
+    "revision": "8fdf2345b7a4e11badcc",
+    "url": "/ClickyGame/static/js/1.8fdf2345.chunk.js"
+  },
+  {
+    "revision": "080ae0317c8ac19e92c0",
+    "url": "/ClickyGame/static/css/main.fa56a0a4.chunk.css"
+  },
+  {
+    "revision": "6ba81935dbfa50a3a5acd519ded14472",
+    "url": "/ClickyGame/index.html"
+  }
+];
